52029e3e3c52eb505b0b88cbe133b9080abf7c81
Mandeep <mandeep.singh@hivehome.com>
2020-03-09 08:53:10 +0000

    Adding const for assertion

--- runtime/const.js
+++ runtime/const.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const Assertion = Object.freeze({
-    'CONTAINS': 'CONTAINS',
-    'IS_EQUAL': 'IS_EQUAL',
-    'IS_NOT_EQUAL': 'IS_NOT_EQUAL',
-    'IS_POPULATED': 'IS_POPULATED',
-    'VALIDATE': 'VALIDATE',
-    'INCREASED': 'INCREASED',
-    'DECREASED': 'DECREASED',
-    'IS_TRUE': 'IS_TRUE',
-    'IS_FALSE': 'IS_FALSE',
+  'CONTAINS': 'CONTAINS',
+  'IS_EQUAL': 'IS_EQUAL',
+  'IS_NOT_EQUAL': 'IS_NOT_EQUAL',
+  'IS_POPULATED': 'IS_POPULATED',
+  'VALIDATE': 'VALIDATE',
+  'INCREASED': 'INCREASED',
+  'DECREASED': 'DECREASED',
+  'IS_TRUE': 'IS_TRUE',
+  'IS_FALSE': 'IS_FALSE',
 });
 
 module.exports = {
-    Assertion,
+  Assertion,
 };
